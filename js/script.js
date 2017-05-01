28bc596c88d3a725294d93aad75db95da763b182
cade <cadekynaston@gmail.com>
2017-04-30 23:09:10 -0600

    making nav animate

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+$(document).on('click','.nav-btn',function(e){
+ //handler code here
+ $header = $(this);
+//getting the next element
+$content = $(".nav-items")
+//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+$content.slideToggle(300, function () {
+    //execute this after slideToggle is done
+    //change text of header based on visibility of content div
+    $header.text(function () {
+        //change text based on condition
+        return $content.is(":visible") ? "hide" : "menu";
+    });
+});
+
+});
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    console.log($("#navBtn"))
+});
