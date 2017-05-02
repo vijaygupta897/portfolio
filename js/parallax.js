0e875ff7cbefaa680279533ef4b3451222b946d9
cade <cadekynaston@gmail.com>
2017-05-02 14:21:56 -0600

    ios fix

--- js/parallax.js
+++ js/parallax.js
@@ -1,9 +1,10 @@
 $(window).scroll(function(){
-
+  if( $(window) .width() > 770){
    var wScroll = $(this).scrollTop();
 
    $('.intro').css({
      'transform' : 'translate(0px, ' + wScroll/3 +'%)'
    });
+ }
 
 });
