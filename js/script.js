efeb2aac13ca126b103fe629e8e69d9078d791b4
cade <cadekynaston@gmail.com>
2017-05-08 12:15:16 -0600

    finished?

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
 
+$(document).ready(function() {
+    $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+});
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "100vw";
@@ -9,3 +46,6 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 
 }
+
+
+// Select all links with hashes
